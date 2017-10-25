a04a9795a9185edaa67171e7d0a1913349c4fd9d
Christophe Eymard <christophe.eymard@sales-way.com>
2017-10-25 15:13:38 +0200

    accept null in attributes

--- src/types.ts
+++ src/types.ts
@@ -44,7 +44,7 @@ export interface ComponentInstanciator<A> {
  * Basic attributes used on all nodes.
  */
 export interface Attrs extends EmptyAttributes {
-  id?: MaybeObservable<string>
+  id?: NullableMaybeObservable<string>
   class?: ArrayOrSingle<ClassDefinition> // special attributes
   style?: StyleDefinition
 
@@ -52,201 +52,205 @@ export interface Attrs extends EmptyAttributes {
   xmlns?: string
 }
 
+
+export type NullableMaybeObservable<T> = MaybeObservable<T | null>
+
+
 export interface HTMLAttributes extends Attrs {
 
   // Attributes shamelessly stolen from React's type definitions.
   // Standard HTML Attributes
-  accept?: MaybeObservable<string>
-  'accept-charset'?: MaybeObservable<string>
-  accesskey?: MaybeObservable<string>
-  action?: MaybeObservable<string>
-  allowfullscreen?: MaybeObservable<boolean>
-  allowtransparency?: MaybeObservable<boolean>
-  alt?: MaybeObservable<string>
-  async?: MaybeObservable<boolean>
-  autocomplete?: MaybeObservable<string>
-  autofocus?: MaybeObservable<boolean>
-  autoplay?: MaybeObservable<boolean>
-  capture?: MaybeObservable<boolean>
-  cellpadding?: MaybeObservable<number | string>
-  cellspacing?: MaybeObservable<number | string>
-  charset?: MaybeObservable<string>
-  challenge?: MaybeObservable<string>
-  checked?: MaybeObservable<boolean>
-  classid?: MaybeObservable<string>
-  classname?: MaybeObservable<string>
-  cols?: MaybeObservable<number>
-  colspan?: MaybeObservable<number>
-  content?: MaybeObservable<string>
-  contenteditable?: MaybeObservable<boolean>
-  contextmenu?: MaybeObservable<string>
-  controls?: MaybeObservable<boolean>
-  coords?: MaybeObservable<string>
-  crossorigin?: MaybeObservable<string>
-  data?: MaybeObservable<string>
-  datetime?: MaybeObservable<string>
-  default?: MaybeObservable<boolean>
-  defer?: MaybeObservable<boolean>
-  dir?: MaybeObservable<string>
-  disabled?: MaybeObservable<boolean>
-  download?: MaybeObservable<any>
-  draggable?: MaybeObservable<boolean>
-  enctype?: MaybeObservable<string>
-  for?: MaybeObservable<string>
-  form?: MaybeObservable<string>
-  formaction?: MaybeObservable<string>
-  formenctype?: MaybeObservable<string>
-  formmethod?: MaybeObservable<string>
-  formnovalidate?: MaybeObservable<boolean>
-  formtarget?: MaybeObservable<string>
-  frameborder?: MaybeObservable<number | string>
-  headers?: MaybeObservable<string>
-  height?: MaybeObservable<number | string>
-  hidden?: MaybeObservable<boolean>
-  high?: MaybeObservable<number>
-  href?: MaybeObservable<string>
-  hreflang?: MaybeObservable<string>
-  htmlfor?: MaybeObservable<string>
-  'http-equiv'?: MaybeObservable<string>
-  icon?: MaybeObservable<string>
-  id?: MaybeObservable<string>
-  inputmode?: MaybeObservable<string>
-  integrity?: MaybeObservable<string>
-  is?: MaybeObservable<string>
-  keyparams?: MaybeObservable<string>
-  keytype?: MaybeObservable<string>
-  kind?: MaybeObservable<string>
-  label?: MaybeObservable<string>
-  lang?: MaybeObservable<string>
-  list?: MaybeObservable<string>
-  loop?: MaybeObservable<boolean>
-  low?: MaybeObservable<number>
-  manifest?: MaybeObservable<string>
-  marginheight?: MaybeObservable<number>
-  marginwidth?: MaybeObservable<number>
-  max?: MaybeObservable<number | string>
-  maxlength?: MaybeObservable<number>
-  media?: MaybeObservable<string>
-  mediagroup?: MaybeObservable<string>
-  method?: MaybeObservable<string>
-  min?: MaybeObservable<number | string>
-  minlength?: MaybeObservable<number>
-  multiple?: MaybeObservable<boolean>
-  muted?: MaybeObservable<boolean>
-  name?: MaybeObservable<string>
-  novalidate?: MaybeObservable<boolean>
-  open?: MaybeObservable<boolean>
-  optimum?: MaybeObservable<number>
-  pattern?: MaybeObservable<string>
-  placeholder?: MaybeObservable<string>
-  poster?: MaybeObservable<string>
-  preload?: MaybeObservable<string>
-  radiogroup?: MaybeObservable<string>
-  readonly?: MaybeObservable<boolean>
-  rel?: MaybeObservable<string>
-  required?: MaybeObservable<boolean>
-  role?: MaybeObservable<string>
-  rows?: MaybeObservable<number>
-  rowspan?: MaybeObservable<number>
-  sandbox?: MaybeObservable<string>
-  scope?: MaybeObservable<string>
-  scoped?: MaybeObservable<boolean>
-  scrolling?: MaybeObservable<string>
-  seamless?: MaybeObservable<boolean>
-  selected?: MaybeObservable<boolean>
-  shape?: MaybeObservable<string>
-  size?: MaybeObservable<number>
-  sizes?: MaybeObservable<string>
-  span?: MaybeObservable<number>
-  spellcheck?: MaybeObservable<boolean>
-  src?: MaybeObservable<string>
-  srcdoc?: MaybeObservable<string>
-  srclang?: MaybeObservable<string>
-  srcset?: MaybeObservable<string>
-  start?: MaybeObservable<number>
-  step?: MaybeObservable<number | string>
-  summary?: MaybeObservable<string>
-  tabindex?: MaybeObservable<number>
-  target?: MaybeObservable<string>
-  title?: MaybeObservable<string>
-  type?: MaybeObservable<string>
-  usemap?: MaybeObservable<string>
-  value?: MaybeObservable<string | number | boolean>
-  width?: MaybeObservable<number | string>
-  wmode?: MaybeObservable<string>
-  wrap?: MaybeObservable<string>
+  accept?: NullableMaybeObservable<string>
+  'accept-charset'?: NullableMaybeObservable<string>
+  accesskey?: NullableMaybeObservable<string>
+  action?: NullableMaybeObservable<string>
+  allowfullscreen?: NullableMaybeObservable<boolean>
+  allowtransparency?: NullableMaybeObservable<boolean>
+  alt?: NullableMaybeObservable<string>
+  async?: NullableMaybeObservable<boolean>
+  autocomplete?: NullableMaybeObservable<string>
+  autofocus?: NullableMaybeObservable<boolean>
+  autoplay?: NullableMaybeObservable<boolean>
+  capture?: NullableMaybeObservable<boolean>
+  cellpadding?: NullableMaybeObservable<number | string>
+  cellspacing?: NullableMaybeObservable<number | string>
+  charset?: NullableMaybeObservable<string>
+  challenge?: NullableMaybeObservable<string>
+  checked?: NullableMaybeObservable<boolean>
+  classid?: NullableMaybeObservable<string>
+  classname?: NullableMaybeObservable<string>
+  cols?: NullableMaybeObservable<number>
+  colspan?: NullableMaybeObservable<number>
+  content?: NullableMaybeObservable<string>
+  contenteditable?: NullableMaybeObservable<boolean>
+  contextmenu?: NullableMaybeObservable<string>
+  controls?: NullableMaybeObservable<boolean>
+  coords?: NullableMaybeObservable<string>
+  crossorigin?: NullableMaybeObservable<string>
+  data?: NullableMaybeObservable<string>
+  datetime?: NullableMaybeObservable<string>
+  default?: NullableMaybeObservable<boolean>
+  defer?: NullableMaybeObservable<boolean>
+  dir?: NullableMaybeObservable<string>
+  disabled?: NullableMaybeObservable<boolean>
+  download?: NullableMaybeObservable<any>
+  draggable?: NullableMaybeObservable<boolean>
+  enctype?: NullableMaybeObservable<string>
+  for?: NullableMaybeObservable<string>
+  form?: NullableMaybeObservable<string>
+  formaction?: NullableMaybeObservable<string>
+  formenctype?: NullableMaybeObservable<string>
+  formmethod?: NullableMaybeObservable<string>
+  formnovalidate?: NullableMaybeObservable<boolean>
+  formtarget?: NullableMaybeObservable<string>
+  frameborder?: NullableMaybeObservable<number | string>
+  headers?: NullableMaybeObservable<string>
+  height?: NullableMaybeObservable<number | string>
+  hidden?: NullableMaybeObservable<boolean>
+  high?: NullableMaybeObservable<number>
+  href?: NullableMaybeObservable<string>
+  hreflang?: NullableMaybeObservable<string>
+  htmlfor?: NullableMaybeObservable<string>
+  'http-equiv'?: NullableMaybeObservable<string>
+  icon?: NullableMaybeObservable<string>
+  id?: NullableMaybeObservable<string>
+  inputmode?: NullableMaybeObservable<string>
+  integrity?: NullableMaybeObservable<string>
+  is?: NullableMaybeObservable<string>
+  keyparams?: NullableMaybeObservable<string>
+  keytype?: NullableMaybeObservable<string>
+  kind?: NullableMaybeObservable<string>
+  label?: NullableMaybeObservable<string>
+  lang?: NullableMaybeObservable<string>
+  list?: NullableMaybeObservable<string>
+  loop?: NullableMaybeObservable<boolean>
+  low?: NullableMaybeObservable<number>
+  manifest?: NullableMaybeObservable<string>
+  marginheight?: NullableMaybeObservable<number>
+  marginwidth?: NullableMaybeObservable<number>
+  max?: NullableMaybeObservable<number | string>
+  maxlength?: NullableMaybeObservable<number>
+  media?: NullableMaybeObservable<string>
+  mediagroup?: NullableMaybeObservable<string>
+  method?: NullableMaybeObservable<string>
+  min?: NullableMaybeObservable<number | string>
+  minlength?: NullableMaybeObservable<number>
+  multiple?: NullableMaybeObservable<boolean>
+  muted?: NullableMaybeObservable<boolean>
+  name?: NullableMaybeObservable<string>
+  novalidate?: NullableMaybeObservable<boolean>
+  open?: NullableMaybeObservable<boolean>
+  optimum?: NullableMaybeObservable<number>
+  pattern?: NullableMaybeObservable<string>
+  placeholder?: NullableMaybeObservable<string>
+  poster?: NullableMaybeObservable<string>
+  preload?: NullableMaybeObservable<string>
+  radiogroup?: NullableMaybeObservable<string>
+  readonly?: NullableMaybeObservable<boolean>
+  rel?: NullableMaybeObservable<string>
+  required?: NullableMaybeObservable<boolean>
+  role?: NullableMaybeObservable<string>
+  rows?: NullableMaybeObservable<number>
+  rowspan?: NullableMaybeObservable<number>
+  sandbox?: NullableMaybeObservable<string>
+  scope?: NullableMaybeObservable<string>
+  scoped?: NullableMaybeObservable<boolean>
+  scrolling?: NullableMaybeObservable<string>
+  seamless?: NullableMaybeObservable<boolean>
+  selected?: NullableMaybeObservable<boolean>
+  shape?: NullableMaybeObservable<string>
+  size?: NullableMaybeObservable<number>
+  sizes?: NullableMaybeObservable<string>
+  span?: NullableMaybeObservable<number>
+  spellcheck?: NullableMaybeObservable<boolean>
+  src?: NullableMaybeObservable<string>
+  srcdoc?: NullableMaybeObservable<string>
+  srclang?: NullableMaybeObservable<string>
+  srcset?: NullableMaybeObservable<string>
+  start?: NullableMaybeObservable<number>
+  step?: NullableMaybeObservable<number | string>
+  summary?: NullableMaybeObservable<string>
+  tabindex?: NullableMaybeObservable<number>
+  target?: NullableMaybeObservable<string>
+  title?: NullableMaybeObservable<string>
+  type?: NullableMaybeObservable<string>
+  usemap?: NullableMaybeObservable<string>
+  value?: NullableMaybeObservable<string | number | boolean>
+  width?: NullableMaybeObservable<number | string>
+  wmode?: NullableMaybeObservable<string>
+  wrap?: NullableMaybeObservable<string>
 
   // RDFa Attributes
-  about?: MaybeObservable<string>
-  datatype?: MaybeObservable<string>
-  inlist?: MaybeObservable<any>
-  prefix?: MaybeObservable<string>
-  property?: MaybeObservable<string>
-  resource?: MaybeObservable<string>
-  typeof?: MaybeObservable<string>
-  vocab?: MaybeObservable<string>
+  about?: NullableMaybeObservable<string>
+  datatype?: NullableMaybeObservable<string>
+  inlist?: NullableMaybeObservable<any>
+  prefix?: NullableMaybeObservable<string>
+  property?: NullableMaybeObservable<string>
+  resource?: NullableMaybeObservable<string>
+  typeof?: NullableMaybeObservable<string>
+  vocab?: NullableMaybeObservable<string>
 
   // Non-standard Attributes
-  autocapitalize?: MaybeObservable<'word' | 'words' | 'sentences' | 'sentence' | 'characters' | 'character' | 'off'>
-  autocorrect?: MaybeObservable<string>
-  autosave?: MaybeObservable<string>
-  color?: MaybeObservable<string>
-  itemprop?: MaybeObservable<string>
-  itemscope?: MaybeObservable<boolean>
-  itemtype?: MaybeObservable<string>
-  itemid?: MaybeObservable<string>
-  itemref?: MaybeObservable<string>
-  results?: MaybeObservable<number>
-  security?: MaybeObservable<string>
-  unselectable?: MaybeObservable<boolean>
+  autocapitalize?: NullableMaybeObservable<'word' | 'words' | 'sentences' | 'sentence' | 'characters' | 'character' | 'off'>
+  autocorrect?: NullableMaybeObservable<string>
+  autosave?: NullableMaybeObservable<string>
+  color?: NullableMaybeObservable<string>
+  itemprop?: NullableMaybeObservable<string>
+  itemscope?: NullableMaybeObservable<boolean>
+  itemtype?: NullableMaybeObservable<string>
+  itemid?: NullableMaybeObservable<string>
+  itemref?: NullableMaybeObservable<string>
+  results?: NullableMaybeObservable<number>
+  security?: NullableMaybeObservable<string>
+  unselectable?: NullableMaybeObservable<boolean>
 }
 
 export interface SVGAttributes extends Attrs {
   'clip-path'?: string;
-  cx?: MaybeObservable<number | string>
-  cy?: MaybeObservable<number | string>
-  d?: MaybeObservable<string>
-  dx?: MaybeObservable<number | string>
-  dy?: MaybeObservable<number | string>
-  fill?: MaybeObservable<string>
-  'fill-opacity'?: MaybeObservable<number | string>
-  'font-family'?: MaybeObservable<string>
-  'font-size'?: MaybeObservable<number | string>
-  fx?: MaybeObservable<number | string>
-  fy?: MaybeObservable<number | string>
-  gradientTransform?: MaybeObservable<string>
-  gradientUnits?: MaybeObservable<string>
-  height?: MaybeObservable<number>
-  'marker-end'?: MaybeObservable<string>
-  'marker-mid'?: MaybeObservable<string>
-  'marker-start'?: MaybeObservable<string>
-  offset?: MaybeObservable<number | string>
-  opacity?: MaybeObservable<number | string>
-  patternContentUnits?: MaybeObservable<string>
-  patternUnits?: MaybeObservable<string>
-  points?: MaybeObservable<string>
-  preserveAspectRatio?: MaybeObservable<string>
-  r?: MaybeObservable<number | string>
-  rx?: MaybeObservable<number | string>
-  ry?: MaybeObservable<number | string>
-  spreadMethod?: MaybeObservable<string>
-  'stop-color'?: MaybeObservable<string>
-  'stop-opacity'?: MaybeObservable<number | string>
-  stroke?: MaybeObservable<string>
-  'stroke-dasharray'?: MaybeObservable<string>
-  'stroke-linecap'?: MaybeObservable<string>
-  'stroke-opacity'?: MaybeObservable<number | string>
-  'stroke-width'?: MaybeObservable<number | string>
-  'text-anchor'?: MaybeObservable<string>
-  'text-decoration'?: MaybeObservable<string>
-  transform?: MaybeObservable<string>
-  version?: MaybeObservable<string>
-  viewBox?: MaybeObservable<string>
-  width?: MaybeObservable<number>
-  x1?: MaybeObservable<number | string>
-  x2?: MaybeObservable<number | string>
-  x?: MaybeObservable<number | string>
-  y1?: MaybeObservable<number | string>
-  y2?: MaybeObservable<number | string>
-  y?: MaybeObservable<number | string>
+  cx?: NullableMaybeObservable<number | string>
+  cy?: NullableMaybeObservable<number | string>
+  d?: NullableMaybeObservable<string>
+  dx?: NullableMaybeObservable<number | string>
+  dy?: NullableMaybeObservable<number | string>
+  fill?: NullableMaybeObservable<string>
+  'fill-opacity'?: NullableMaybeObservable<number | string>
+  'font-family'?: NullableMaybeObservable<string>
+  'font-size'?: NullableMaybeObservable<number | string>
+  fx?: NullableMaybeObservable<number | string>
+  fy?: NullableMaybeObservable<number | string>
+  gradientTransform?: NullableMaybeObservable<string>
+  gradientUnits?: NullableMaybeObservable<string>
+  height?: NullableMaybeObservable<number>
+  'marker-end'?: NullableMaybeObservable<string>
+  'marker-mid'?: NullableMaybeObservable<string>
+  'marker-start'?: NullableMaybeObservable<string>
+  offset?: NullableMaybeObservable<number | string>
+  opacity?: NullableMaybeObservable<number | string>
+  patternContentUnits?: NullableMaybeObservable<string>
+  patternUnits?: NullableMaybeObservable<string>
+  points?: NullableMaybeObservable<string>
+  preserveAspectRatio?: NullableMaybeObservable<string>
+  r?: NullableMaybeObservable<number | string>
+  rx?: NullableMaybeObservable<number | string>
+  ry?: NullableMaybeObservable<number | string>
+  spreadMethod?: NullableMaybeObservable<string>
+  'stop-color'?: NullableMaybeObservable<string>
+  'stop-opacity'?: NullableMaybeObservable<number | string>
+  stroke?: NullableMaybeObservable<string>
+  'stroke-dasharray'?: NullableMaybeObservable<string>
+  'stroke-linecap'?: NullableMaybeObservable<string>
+  'stroke-opacity'?: NullableMaybeObservable<number | string>
+  'stroke-width'?: NullableMaybeObservable<number | string>
+  'text-anchor'?: NullableMaybeObservable<string>
+  'text-decoration'?: NullableMaybeObservable<string>
+  transform?: NullableMaybeObservable<string>
+  version?: NullableMaybeObservable<string>
+  viewBox?: NullableMaybeObservable<string>
+  width?: NullableMaybeObservable<number>
+  x1?: NullableMaybeObservable<number | string>
+  x2?: NullableMaybeObservable<number | string>
+  x?: NullableMaybeObservable<number | string>
+  y1?: NullableMaybeObservable<number | string>
+  y2?: NullableMaybeObservable<number | string>
+  y?: NullableMaybeObservable<number | string>
 }
