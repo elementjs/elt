17cbca767fd109a544ca653cc9071b816b4f0141
Christophe Eymard <christophe.eymard@sales-way.com>
2018-12-12 14:46:33 +0100

    fixed some compilation issues with newer typescript

--- src/types.ts
+++ src/types.ts
@@ -5,7 +5,7 @@ import {
 import {Mixin} from './mixins'
 
 
-export type Listener<EventType extends Event, N extends Node = Node> = (this: N, ev: EventType, node: N) => void
+export type Listener<EventType extends Event, N extends Node = Node> = (this: N, ev: EventType, node: N, mixin: Mixin<N>) => void
 
 
 export interface ComponentInterface<A> {
