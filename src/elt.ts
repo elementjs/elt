5fef70ba76024dfbf340b1a7b65be0fb9502eb8d
Christophe Eymard <christophe.eymard@sales-way.com>
2019-12-12 17:32:08 +0100

    moved the JSX namespace

--- src/elt.ts
+++ src/elt.ts
@@ -108,6 +108,7 @@ function isComponent(kls: any): kls is new (attrs: Attrs) => Component<any> {
   return kls.prototype instanceof Component
 }
 
+
 export const GLOBAL_ATTRIBUTES = {
   accesskey: true,
   contenteditable: true,
@@ -228,199 +229,200 @@ export type ElementAlias = Element
 import { Fragment as F } from './verbs'
 
 export namespace e {
+
+  /**
+   * Extend the JSX namespace to be able to use .tsx code.
+   */
+  export namespace JSX {
+    export type Element = ElementAlias
+
+    export interface ElementAttributesProperty {
+      attrs: any
+    }
+
+    export interface ElementChildrenAttribute {
+      $$children: any
+    }
+
+    export interface ElementClassFn {
+      (attrs: EmptyAttributes, children: DocumentFragment): Element
+    }
+
+    export type ElementClass = ElementClassFn | Component
+
+    export interface IntrinsicElements {
+      a: HTMLAttributes
+      abbr: HTMLAttributes
+      address: HTMLAttributes
+      area: HTMLAttributes
+      article: HTMLAttributes
+      aside: HTMLAttributes
+      audio: HTMLAttributes
+      b: HTMLAttributes
+      base: HTMLAttributes
+      bdi: HTMLAttributes
+      bdo: HTMLAttributes
+      big: HTMLAttributes
+      blockquote: HTMLAttributes
+      body: HTMLAttributes
+      br: HTMLAttributes
+      button: HTMLAttributes
+      canvas: HTMLAttributes
+      caption: HTMLAttributes
+      cite: HTMLAttributes
+      code: HTMLAttributes
+      col: HTMLAttributes
+      colgroup: HTMLAttributes
+      data: HTMLAttributes
+      datalist: HTMLAttributes
+      dd: HTMLAttributes
+      del: HTMLAttributes
+      details: HTMLAttributes
+      dfn: HTMLAttributes
+      dialog: HTMLAttributes
+      div: HTMLAttributes
+      dl: HTMLAttributes
+      dt: HTMLAttributes
+      em: HTMLAttributes
+      embed: HTMLAttributes
+      fieldset: HTMLAttributes
+      figcaption: HTMLAttributes
+      figure: HTMLAttributes
+      footer: HTMLAttributes
+      form: HTMLAttributes
+      h1: HTMLAttributes
+      h2: HTMLAttributes
+      h3: HTMLAttributes
+      h4: HTMLAttributes
+      h5: HTMLAttributes
+      h6: HTMLAttributes
+      head: HTMLAttributes
+      header: HTMLAttributes
+      hr: HTMLAttributes
+      html: HTMLAttributes
+      i: HTMLAttributes
+      iframe: HTMLAttributes
+      img: HTMLAttributes
+      input: HTMLAttributes
+      ins: HTMLAttributes
+      kbd: HTMLAttributes
+      keygen: HTMLAttributes
+      label: HTMLAttributes
+      legend: HTMLAttributes
+      li: HTMLAttributes
+      link: HTMLAttributes
+      main: HTMLAttributes
+      map: HTMLAttributes
+      mark: HTMLAttributes
+      menu: HTMLAttributes
+      menuitem: HTMLAttributes
+      meta: HTMLAttributes
+      meter: HTMLAttributes
+      nav: HTMLAttributes
+      noscript: HTMLAttributes
+      object: HTMLAttributes
+      ol: HTMLAttributes
+      optgroup: HTMLAttributes
+      option: HTMLAttributes
+      output: HTMLAttributes
+      p: HTMLAttributes
+      param: HTMLAttributes
+      picture: HTMLAttributes
+      pre: HTMLAttributes
+      progress: HTMLAttributes
+      q: HTMLAttributes
+      rp: HTMLAttributes
+      rt: HTMLAttributes
+      ruby: HTMLAttributes
+      s: HTMLAttributes
+      samp: HTMLAttributes
+      script: HTMLAttributes
+      section: HTMLAttributes
+      select: HTMLAttributes
+      small: HTMLAttributes
+      source: HTMLAttributes
+      span: HTMLAttributes
+      strong: HTMLAttributes
+      style: HTMLAttributes
+      sub: HTMLAttributes
+      summary: HTMLAttributes
+      sup: HTMLAttributes
+      table: HTMLAttributes
+      tbody: HTMLAttributes
+      td: HTMLAttributes
+      textarea: HTMLAttributes
+      tfoot: HTMLAttributes
+      th: HTMLAttributes
+      thead: HTMLAttributes
+      time: HTMLAttributes
+      title: HTMLAttributes
+      tr: HTMLAttributes
+      track: HTMLAttributes
+      u: HTMLAttributes
+      ul: HTMLAttributes
+      'var': HTMLAttributes
+      video: HTMLAttributes
+      wbr: HTMLAttributes
+
+      svg: SVGAttributes
+      circle: SVGAttributes
+      clipPath: SVGAttributes
+      defs: SVGAttributes
+      desc: SVGAttributes
+      ellipse: SVGAttributes
+      feBlend: SVGAttributes
+      feColorMatrix: SVGAttributes
+      feComponentTransfer: SVGAttributes
+      feComposite: SVGAttributes
+      feConvolveMatrix: SVGAttributes
+      feDiffuseLighting: SVGAttributes
+      feDisplacementMap: SVGAttributes
+      feDistantLight: SVGAttributes
+      feFlood: SVGAttributes
+      feFuncA: SVGAttributes
+      feFuncB: SVGAttributes
+      feFuncG: SVGAttributes
+      feFuncR: SVGAttributes
+      feGaussianBlur: SVGAttributes
+      feImage: SVGAttributes
+      feMerge: SVGAttributes
+      feMergeNode: SVGAttributes
+      feMorphology: SVGAttributes
+      feOffset: SVGAttributes
+      fePointLight: SVGAttributes
+      feSpecularLighting: SVGAttributes
+      feSpotLight: SVGAttributes
+      feTile: SVGAttributes
+      feTurbulence: SVGAttributes
+      filter: SVGAttributes
+      foreignObject: SVGAttributes
+      g: SVGAttributes
+      image: SVGAttributes
+      line: SVGAttributes
+      linearGradient: SVGAttributes
+      marker: SVGAttributes
+      mask: SVGAttributes
+      metadata: SVGAttributes
+      path: SVGAttributes
+      pattern: SVGAttributes
+      polygon: SVGAttributes
+      polyline: SVGAttributes
+      radialGradient: SVGAttributes
+      rect: SVGAttributes
+      stop: SVGAttributes
+      switch: SVGAttributes
+      symbol: SVGAttributes
+      text: SVGAttributes
+      textPath: SVGAttributes
+      tspan: SVGAttributes
+      use: SVGAttributes
+      view: SVGAttributes
+
+    }
+  }
+
   export const createElement = e //(elt: ComponentFn, attrs: Attrs, ...children: Insertable[]): Node
   // export const createElement(elt: string, attrs: Attrs, ...children: Insertable[]): HTMLElement
   // export const createElement<A>(elt: ComponentInstanciator<A>, attrs: A, ...children: Insertable[]): Node
-  export const Fragment: (at: Attrs, ch: DocumentFragment) => e.JSX.Element = F //(at: Attrs, ch: DocumentFragment): e.JSX.Element
-}
-
-/**
- * Extend the JSX namespace to be able to use .tsx code.
- */
-export namespace e.JSX {
-  export type Element = ElementAlias
-
-  export interface ElementAttributesProperty {
-    attrs: any
-  }
-
-  export interface ElementChildrenAttribute {
-    $$children: any
-  }
-
-  export interface ElementClassFn {
-    (attrs: EmptyAttributes, children: DocumentFragment): Element
-  }
-
-  export type ElementClass = ElementClassFn | Component
-
-  export interface IntrinsicElements {
-    a: HTMLAttributes
-    abbr: HTMLAttributes
-    address: HTMLAttributes
-    area: HTMLAttributes
-    article: HTMLAttributes
-    aside: HTMLAttributes
-    audio: HTMLAttributes
-    b: HTMLAttributes
-    base: HTMLAttributes
-    bdi: HTMLAttributes
-    bdo: HTMLAttributes
-    big: HTMLAttributes
-    blockquote: HTMLAttributes
-    body: HTMLAttributes
-    br: HTMLAttributes
-    button: HTMLAttributes
-    canvas: HTMLAttributes
-    caption: HTMLAttributes
-    cite: HTMLAttributes
-    code: HTMLAttributes
-    col: HTMLAttributes
-    colgroup: HTMLAttributes
-    data: HTMLAttributes
-    datalist: HTMLAttributes
-    dd: HTMLAttributes
-    del: HTMLAttributes
-    details: HTMLAttributes
-    dfn: HTMLAttributes
-    dialog: HTMLAttributes
-    div: HTMLAttributes
-    dl: HTMLAttributes
-    dt: HTMLAttributes
-    em: HTMLAttributes
-    embed: HTMLAttributes
-    fieldset: HTMLAttributes
-    figcaption: HTMLAttributes
-    figure: HTMLAttributes
-    footer: HTMLAttributes
-    form: HTMLAttributes
-    h1: HTMLAttributes
-    h2: HTMLAttributes
-    h3: HTMLAttributes
-    h4: HTMLAttributes
-    h5: HTMLAttributes
-    h6: HTMLAttributes
-    head: HTMLAttributes
-    header: HTMLAttributes
-    hr: HTMLAttributes
-    html: HTMLAttributes
-    i: HTMLAttributes
-    iframe: HTMLAttributes
-    img: HTMLAttributes
-    input: HTMLAttributes
-    ins: HTMLAttributes
-    kbd: HTMLAttributes
-    keygen: HTMLAttributes
-    label: HTMLAttributes
-    legend: HTMLAttributes
-    li: HTMLAttributes
-    link: HTMLAttributes
-    main: HTMLAttributes
-    map: HTMLAttributes
-    mark: HTMLAttributes
-    menu: HTMLAttributes
-    menuitem: HTMLAttributes
-    meta: HTMLAttributes
-    meter: HTMLAttributes
-    nav: HTMLAttributes
-    noscript: HTMLAttributes
-    object: HTMLAttributes
-    ol: HTMLAttributes
-    optgroup: HTMLAttributes
-    option: HTMLAttributes
-    output: HTMLAttributes
-    p: HTMLAttributes
-    param: HTMLAttributes
-    picture: HTMLAttributes
-    pre: HTMLAttributes
-    progress: HTMLAttributes
-    q: HTMLAttributes
-    rp: HTMLAttributes
-    rt: HTMLAttributes
-    ruby: HTMLAttributes
-    s: HTMLAttributes
-    samp: HTMLAttributes
-    script: HTMLAttributes
-    section: HTMLAttributes
-    select: HTMLAttributes
-    small: HTMLAttributes
-    source: HTMLAttributes
-    span: HTMLAttributes
-    strong: HTMLAttributes
-    style: HTMLAttributes
-    sub: HTMLAttributes
-    summary: HTMLAttributes
-    sup: HTMLAttributes
-    table: HTMLAttributes
-    tbody: HTMLAttributes
-    td: HTMLAttributes
-    textarea: HTMLAttributes
-    tfoot: HTMLAttributes
-    th: HTMLAttributes
-    thead: HTMLAttributes
-    time: HTMLAttributes
-    title: HTMLAttributes
-    tr: HTMLAttributes
-    track: HTMLAttributes
-    u: HTMLAttributes
-    ul: HTMLAttributes
-    'var': HTMLAttributes
-    video: HTMLAttributes
-    wbr: HTMLAttributes
-
-    svg: SVGAttributes
-    circle: SVGAttributes
-    clipPath: SVGAttributes
-    defs: SVGAttributes
-    desc: SVGAttributes
-    ellipse: SVGAttributes
-    feBlend: SVGAttributes
-    feColorMatrix: SVGAttributes
-    feComponentTransfer: SVGAttributes
-    feComposite: SVGAttributes
-    feConvolveMatrix: SVGAttributes
-    feDiffuseLighting: SVGAttributes
-    feDisplacementMap: SVGAttributes
-    feDistantLight: SVGAttributes
-    feFlood: SVGAttributes
-    feFuncA: SVGAttributes
-    feFuncB: SVGAttributes
-    feFuncG: SVGAttributes
-    feFuncR: SVGAttributes
-    feGaussianBlur: SVGAttributes
-    feImage: SVGAttributes
-    feMerge: SVGAttributes
-    feMergeNode: SVGAttributes
-    feMorphology: SVGAttributes
-    feOffset: SVGAttributes
-    fePointLight: SVGAttributes
-    feSpecularLighting: SVGAttributes
-    feSpotLight: SVGAttributes
-    feTile: SVGAttributes
-    feTurbulence: SVGAttributes
-    filter: SVGAttributes
-    foreignObject: SVGAttributes
-    g: SVGAttributes
-    image: SVGAttributes
-    line: SVGAttributes
-    linearGradient: SVGAttributes
-    marker: SVGAttributes
-    mask: SVGAttributes
-    metadata: SVGAttributes
-    path: SVGAttributes
-    pattern: SVGAttributes
-    polygon: SVGAttributes
-    polyline: SVGAttributes
-    radialGradient: SVGAttributes
-    rect: SVGAttributes
-    stop: SVGAttributes
-    switch: SVGAttributes
-    symbol: SVGAttributes
-    text: SVGAttributes
-    textPath: SVGAttributes
-    tspan: SVGAttributes
-    use: SVGAttributes
-    view: SVGAttributes
-
-  }
+  export const Fragment: (at: Attrs, ch: DocumentFragment) => JSX.Element = F //(at: Attrs, ch: DocumentFragment): e.JSX.Element
 }
