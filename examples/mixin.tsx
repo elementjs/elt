ddf6ed44de2f2c158e76b3985747cd1efea19f8b
Christophe Eymard <christophe.eymard@sales-way.com>
2020-06-03 17:30:45 +0200

    more details on mixin

--- examples/mixin.tsx
+++ examples/mixin.tsx
@@ -1,14 +1,35 @@
 import { Mixin } from 'elt'
 
+/**
+ * Always allow for the node type to be infered by specifying
+ * it as a type parameter when creating a Mixin class.
+ */
 class MyMixinWorks<N extends HTMLElement> extends Mixin<N> {
-
-}
-
-class MyMixinFails extends Mixin<HTMLElement> {
 
 }
 
 var div = <div>
   {new MyMixinWorks()}
+</div>
+
+var mx = new MyMixinWorks<HTMLDivElement>()
+var div2 = <div>{mx}</div>
+
+var mx2 = new MyMixinWorks()
+// Typescript can't figure that one out, yet ; waiting for https://github.com/Microsoft/TypeScript/issues/14520
+// to be resolved.
+var div3 = <div>{mx2}</div>
+
+/**
+ * Locking a Mixin to a single node type will prevent it from being
+ * used on one of its children node type because typescript currently
+ * does not allow generics lower bounds checking.
+ */
+class MyMixinFails extends Mixin<HTMLElement> {
+
+}
+
+// doesn't work either, even though it should.
+var div4 = <div>
   {new MyMixinFails()}
 </div>
--- examples/node_add_mixin.tsx
+++ examples/node_add_mixin.tsx
@@ -1,12 +1,15 @@
 import { Mixin, node_add_mixin } from 'elt'
 
-class SomeMixin extends Mixin<Node> {
+class SomeMixin<N extends Node> extends Mixin<N> {
 
 }
 
-var my_mixin = new SomeMixin()
+// If instanciated before, the inferer will choke if the node type
+// is not exactly what the mixin will be assigned to. So we *have*
+// to manually instanciate it here.
+var my_mixin = new SomeMixin<HTMLDivElement>()
 
-// these are equivalent
+// these two statements are mostly equivalent.
 var n = <div>{my_mixin}</div>
 
 var d = <div/>
