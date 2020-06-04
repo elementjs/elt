e3ddd59f1f24a428877ad0b7675de39ecfb818d6
Christophe Eymard <christophe.eymard@sales-way.com>
2020-06-04 10:37:07 +0200

    more details in app.displayapp example

--- examples/app.displayapp.tsx
+++ examples/app.displayapp.tsx
@@ -1,16 +1,33 @@
-import { App } from 'elt'
+import { App, o, $bind, $click } from 'elt'
 
-function SomeLoginForm() { return <div></div> }
+class AppMain extends App.Service {
+  @App.view
+  Main() {
+    return <div>Logged in !</div>
+  }
+}
 
 class LoginService extends App.Service {
   @App.view
   Main() {
+    const o_user = o('user')
     return <div>
-      <SomeLoginForm/>
+      <input placeholder='username'>
+        {$bind.string(o_user)}
+      </input>
+      <button>
+        Login
+        {$click(_ => {
+          // validate the login or display an error.
+          // if the credentials are correct, we can change the current active service.
+          this.app.activate(AppMain)
+        })}
+      </button>
     </div>
   }
 }
 
 document.body.appendChild(
+  // We display the Main view from the first activated service that has it.
   App.DisplayApp('Main', LoginService)
 )
