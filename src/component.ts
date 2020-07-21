e56ecd668833cc2175f1876c42b5ea4dfebf79bd
Christophe Eymard <christophe.eymard@sales-way.com>
2020-07-21 17:35:34 +0200

    Component definition shouldn't have to define node type twice

--- src/component.ts
+++ src/component.ts
@@ -1,6 +1,6 @@
 
 import { node_add_data } from './dom'
-import type { EmptyAttributes, Attrs, Renderable } from './elt'
+import type { EmptyAttributes, Attrs, Renderable, AttrsNodeType } from './elt'
 
 
 /**
@@ -12,11 +12,11 @@ import type { EmptyAttributes, Attrs, Renderable } from './elt'
  * All attributes **must** extend the base `Attrs` class.
  * @category dom, toc
  */
-export abstract class Component<N extends Node, A extends EmptyAttributes<N> = Attrs<N>> {
+export abstract class Component<A extends EmptyAttributes<any> = Attrs<HTMLElement>> {
   /**
    *
    */
-  node!: N
+  node!: AttrsNodeType<A>
   /**
    * The attributes passed to the component
    */
@@ -30,10 +30,10 @@ export abstract class Component<N extends Node, A extends EmptyAttributes<N> = A
   /**
    * The render function that has to be defined by Components
    */
-  abstract render(children: Renderable[]): N
+  abstract render(children: Renderable[]): AttrsNodeType<A>
 
   /** @internal */
-  renderAndAttach(children: Renderable[]): N {
+  renderAndAttach(children: Renderable[]): AttrsNodeType<A> {
     const res = this.render(children)
     this.node = res
     node_add_data(res, this)
