1cd4673ae7d02d3cd0365dfe164ac3ec295a3f57
Christophe Eymard <christophe.eymard@sales-way.com>
2023-12-11 22:28:47 +0100

    slightly changed escaping

--- src/app.ts
+++ src/app.ts
@@ -19,28 +19,32 @@ function _assign<B extends Map<any, any> | {[name: string]: string}>(base: B, ..
 
 function _decode(s: string): string | boolean | undefined | number | null {
   let val: string | boolean | undefined | number | null = s
-  if (s[1] === "~") {
-    val = val.slice(1)
-  } else if (s[1] === "n") {
-    val = null
-  } else if (s[1] === "u") {
-    val = undefined
-  } else if (s[1] === "f") {
-    val = false
-  } else if (s[1] === "t") {
-    val = true
-  } else if (/^[.0-9-]/.test(s[1])) {
-    val = parseFloat(s.slice(1))
+  if (/^[.0-9-]/.test(s[0])) {
+    val = parseFloat(val)
+  } else if (s[0] === "~") {
+    if (s[1] === "n") {
+      val = null
+    } else if (s[1] === "u") {
+      val = undefined
+    } else if (s[1] === "f") {
+      val = false
+    } else if (s[1] === "t") {
+      val = true
+    } else {
+      val = val.slice(1) // we have a string that started by a special character
+    }
   }
   return val
 }
 
 function _encode(v: string | boolean | undefined | number | null): string {
   // encode value and its basic type in the URL
-  if (typeof v === "string" && v[0] === "~") {
+  if (typeof v === "string" && /[~.0-9-]/.test(v[0])) {
+    // We only need to test for the ~ and numbers, since this is the only
+    // way for a string to start with a forbidden character
     v = "~" + v
   } else if (typeof v === "number") {
-    v = "~" + v
+    v = v.toString()
   } else if (v === true) {
     v = "~t"
   } else if (v === false) {
