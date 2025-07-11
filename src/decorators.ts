f8c2c013a3317aef1f18782b3878bb022e4cc9ce
Christophe Eymard <christophe.eymard@sales-way.com>
2025-07-11 11:40:52 +0200

    CSSBuilder now uses CSSStylesheet

--- src/decorators.ts
+++ src/decorators.ts
@@ -22,9 +22,6 @@ import type {
   StyleDefinition,
 } from "./types"
 
-import { css } from "./css"
-
-
 // FIXME this lacks some debounce and throttle, or a way of achieving it.
 function setup_bind<T, N extends Element>(
   obs: o.IObservable<T | null | undefined, T>,
@@ -341,8 +338,6 @@ export function $shadow(opts?: Node | $ShadowOptions, child?: Node) {
     }
   }
 }
-
-$shadow.css = css
 
 
 /**
