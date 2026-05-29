95be21c1d58d8bbaa582e3914144418fff3c0f7c
Christophe Eymard <ceymard@pm.me>
2026-05-29 18:22:44 +0200

    comments in usage

--- demo/src/screen-ui-usage.tsx
+++ demo/src/screen-ui-usage.tsx
@@ -13,6 +13,15 @@ export default class ScreenUIUsage extends App.Service.requirements(() => ({
 
       <pre><code>import "elt/ui/auto"</code></pre>
 
+      <h3>Design rules</h3>
+
+      <ul>
+        <li>The theme engine provides colors, spacing and border radius values. Avoid using other values.</li>
+        <li>In general, <strong>avoid using margins</strong>; leave spacing to e-flex with its gap, or e-box in typographic mode. If using margins, no two elements should follow one another with a margin on - only the greater of the two must appear.</li>
+        <li>Only use two font weights: regular and bold (exact values depend on the font.)</li>
+        <li>For regular text, try to use only two or three font sizes.</li>
+      </ul>
+
       <h3>Principles and visual language</h3>
 
       <p>As with the core elt library, elt/ui strives to give much with little. Instead of providing many widgets, it defines a few elements and facilities to build your own, following a visual language that tries to be minimalistic, consistent and predictable.</p>
